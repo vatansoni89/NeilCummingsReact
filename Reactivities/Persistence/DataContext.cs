cd74a6efffb24992bf1a8c3e544af43f6c97e122
Vatan Soni <vatansoni89@gmail.com>
2019-10-23 17:51:01 +0530

    seeding data,  and updated get value controller

--- Reactivities/Persistence/DataContext.cs
+++ Reactivities/Persistence/DataContext.cs
@@ -10,5 +10,11 @@ namespace Persistence
         {
         }
         public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Value>().HasData(new Value { Id = 101, Name = "Name101" },
+            new Value { Id = 102, Name = "Name102" });
+        }
     }
 }
