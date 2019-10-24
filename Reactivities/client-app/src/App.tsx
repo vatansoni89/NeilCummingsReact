00a0818f5b1bad6998fbe79ef08620f020f493ae
Vatan Soni <vatansoni89@gmail.com>
2019-10-25 00:43:09 +0530

    Axios and CORS

--- Reactivities/client-app/src/App.tsx
+++ Reactivities/client-app/src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { values: [] };
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/values/").then(response => {
+      this.setState({
+        values: response.data
+      });
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <ul>
+            {this.state.values.map((val: any) => (
+              <li key={val.id}>
+                {val.id}: {val.name}
+              </li>
+            ))}
+          </ul>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
